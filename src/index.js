fa4eb238bc940231b8908e4a8a37e523d55872fd
fmfahath.testLab <fmfahath.dev@gmail.com>
2023-02-15 14:27:46 +0545

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+ import React from "react";
+ import reactDOM from "react-dom";
+
+const element = <h1>Hello react</h1>
+// console.log(element);
+reactDOM.render(element,document.getElementById('root'));
